603e72737a551c6411fd72fa5d490d202aa27598
CyrilusK <kardash199696@mail.ru>
2019-03-10 18:56:48 +0300

    bug fixes

--- hello_world.cpp
+++ hello_world.cpp
@@ -1,8 +1,12 @@
 include <iostream>
 include <string>
 using namespace std;
+int main()
+{
 string name;
 cout<<"What is your name?";
 cin>>name;
 cout<<"Hello World from "<<name;
+return 0;
+}
 # homework2
