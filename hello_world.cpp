f2d494e484480be908152bd043af25ccec7b5c4c
CyrilusK <kardash199696@mail.ru>
2019-03-10 19:18:01 +0300

    comments added

--- hello_world.cpp
+++ hello_world.cpp
@@ -3,10 +3,10 @@ include <string>
 using namespace std;
 int main()
 {
-string name;
+string name; //объявление переменной имени пользователя
 cout<<"What is your name?";
-cin>>name;
-cout<<"Hello World from "<<name;
+cin>>name; //ввод имени пользователя
+cout<<"Hello World from "<<name; //вывод приветствия с именем пользователя
 return 0;
 }
 # homework2
