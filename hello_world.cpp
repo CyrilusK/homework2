84ecfb97669d0395534c84b6bb50ed5ea699515f
CyrilusK <47518991+CyrilusK@users.noreply.github.com>
2019-03-10 20:36:50 +0300

    Update hello_world.cpp
    Внёс изменения в комментарии к коду

--- hello_world.cpp
+++ hello_world.cpp
@@ -3,10 +3,10 @@ include <string>
 using namespace std;
 int main()
 {
-string name; //объявление переменной имени пользователя
+string name; //Объявление переменной имени пользователя.
 cout<<"What is your name?";
-cin>>name; //ввод имени пользователя
-cout<<"Hello World from "<<name; //вывод приветствия с именем пользователя
+cin>>name; //Ввод имени пользователя.
+cout<<"Hello World from "<<name; //Вывод приветствия с именем пользователя.
 return 0;
 }
 # homework2
